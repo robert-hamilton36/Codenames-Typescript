ae07b106316b1d96166746668c8fc34521db602c
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-06-13 21:44:45 +1200

    add firestore joingame actions

--- client/contexts/FirebaseContext.tsx
+++ client/contexts/FirebaseContext.tsx
@@ -2,10 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react'
+import { joinGameActions } from '../firebase/joinGameActions'
 interface IFirebase {
-  app: firebase.app.App,
-  firestore: firebase.firestore.Firestore
+  app: app,
+  firestore: firestore
 }
+
+export type firestore = firebase.firestore.Firestore
+export type app = firebase.app.App
 
 const FirebaseContext = createContext<IFirebase | null >(null)
 
@@ -27,7 +31,8 @@ export const FirebaseProvider: React.FC<React.ReactNode> = ({ children }) => {
     return firebase.initializeApp(firebaseConfig)
   }, [firebaseConfig])
   const firestore = app.firestore()
-  const provided: IFirebase = useMemo(() => ({ app, firestore }), [app, firestore])
+  const actions = joinGameActions(firestore)
+  const provided: IFirebase = useMemo(() => ({ app, firestore, actions }), [app, firestore, actions])
 
   return (
     <FirebaseContext.Provider value={provided}>
--- client/firebase/joinGameActions.tsx
+++ client/firebase/joinGameActions.tsx
@@ -1,12 +1,12 @@
+import { useUserContext } from '../contexts/UserContext'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 // eslint-disable-next-line import/no-unresolved
 import { GameState, PlayerObject, TeamColour, User } from '../types/gameState'
+import { firestore } from '../contexts/FirebaseContext'
 
-const firestore = firebase.firestore()
-
-const getNewPlayerTeam: (data:Data) => TeamColour = (data) => {
+const getNewPlayersTeam: (data:Data) => TeamColour = (data) => {
   let team: TeamColour = ''
   if (data.data().settings.gameplayMode !== 'tabletop') {
     if (data.data().players.filter((person) => person.team === 'Red').length <= data.data().players.filter((person) => person.team === 'Blue').length) {
@@ -18,24 +18,44 @@ const getNewPlayerTeam: (data:Data) => TeamColour = (data) => {
   return team
 }
 
-export const createGame:(newGame: GameState) => FirestorePromise = (newGame: GameState) => {
-  return firestore.collection('Games')
-    .add(newGame)
+export const joinGameActions: (firestore: firestore) => ActionReturns = (firestore: firestore) => {
+  const { setGameId } = useUserContext()
+
+  const consoleLog = () => {
+    console.log(firestore)
+  }
+
+  const createGame: (newGame: GameState) => Promise<void> = (newGame: GameState) => {
+    return firestore.collection('Games')
+      .add(newGame)
+      .then(data => setGameId(data.id))
+  }
+
+  const joinGame: JoinGame = (user: User, gameId:string, firestore: firestore) => {
+    const ref = firestore.collection('Games').doc(gameId)
+    return firestore.runTransaction((transaction) => {
+      return transaction.get(ref)
+        .then(data => {
+          const team: TeamColour = getNewPlayersTeam(data)
+          const playerObj: PlayerObject = { ...user, team }
+          transaction.update(ref, { players: firebase.firestore.FieldValue.arrayUnion(playerObj) })
+          return data
+        })
+    })
+  }
+  return {
+    consoleLog,
+    createGame,
+    joinGame
+  }
 }
 
-export const joinGame: (user: User, gameId:string) => FirestoreTransactionPromise = (user: User, gameId:string) => {
-  const ref = firestore.collection('Games').doc(gameId)
-  return firestore.runTransaction((transaction) => {
-    return transaction.get(ref)
-      .then(data => {
-        const team: TeamColour = getNewPlayerTeam(data)
-        const playerObj: PlayerObject = { ...user, team }
-        transaction.update(ref, { players: firebase.firestore.FieldValue.arrayUnion(playerObj) })
-        return data
-      })
-  })
-}
-
-type FirestorePromise = Promise<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>>
+type JoinGame = (user: User, gameId: string, firestore: firebase.firestore.Firestore) => FirestoreTransactionPromise
 type FirestoreTransactionPromise = Promise<firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>>
 type Data = firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>
+
+interface ActionReturns {
+  consoleLog: () => void,
+  createGame: (newGame: GameState) => Promise<void>,
+  joinGame: JoinGame
+}

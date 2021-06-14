9c7b2e93092dca6c83dbe0092de8894ae6fc28f6
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-06-14 12:08:08 +1200

    add the testing component hello

--- client/components/Hello.tsx
+++ client/components/Hello.tsx
@@ -1,25 +1,40 @@
 import React from 'react'
-// import { createGame } from '../firebase'
+import { useFirebase, useFirestoreSubscriber } from '../contexts/FirebaseContext'
 
 import { useUserContext } from '../contexts/UserContext'
-import { joinGame } from '../firebase/firebase'
 
 export const Hello: React.FC = () => {
-  const { user, setUser, gameId, setGameId } = useUserContext()
-  // const handleClick = async () => {
-  //   createGame({ players: ['host'] })
-  //     .then(data => setGameId(data.id))
-  //     .catch(error => console.log(error))
+  const { user, setUser, gameId } = useUserContext()
+  const { actions } = useFirebase()
 
-  // }
-  const handleClick = async () => {
-    joinGame(user, 'uaR5XYbPZv5L54Zyibjq')
-      .then(data => console.log(data))
-      .catch(error => console.log(error))
+  const data = useFirestoreSubscriber(gameId)
+
+  const handleClick = () => {
+    actions.createGame({ players: [user], settings: { gameplayMode: 'individual' } })
+  }
+
+  const handleJoin = () => {
+    actions.joinGame({ name: 'Obi Wan', uid: '34342-fad-231' }, gameId)
+  }
+
+  const handleKick = () => {
+    actions.kickPlayer('34342-fad-231', gameId)
+  }
+
+  const handleLeave = () => {
+    actions.leaveGame(user.uid, gameId)
+  }
+
+  const handleDelete = () => {
+    actions.deleteGame(gameId)
   }
 
   const handleLogin = () => {
     setUser('Anakin')
+  }
+
+  const handleLogOut = () => {
+    setUser('')
   }
   return (
     <>
@@ -28,8 +43,14 @@ export const Hello: React.FC = () => {
       {user && <h1>Hello { user.name } </h1>}
       {user && <h1>{user.uid}</h1>}
       {gameId && <h1>{gameId}</h1>}
+      {data && data.players.map((player) => <h1 key={player.uid}>{player.name}</h1>)}
       <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogOut}>LogOut</button>
       <button onClick={handleClick}>Create game</button>
+      <button onClick={handleJoin}>Join</button>
+      <button onClick={handleKick}>Kick</button>
+      <button onClick={handleLeave}>Leave</button>
+      <button onClick={handleDelete}>Delete</button>
     </>
   )
 }

6474387589b748e91f74ff22b19f14a6bfc4d46f
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-06-14 15:30:38 +1200

    add test fot user provider

---
+++ client/contexts/UserContext.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import { UserProvider, useUserContext } from './UserContext'
+
+test('Should set gameId and user, and read those values', () => {
+  const wrapper = ({ children }) => <UserProvider> {children} </UserProvider>
+  const { result } = renderHook(() => useUserContext(), { wrapper })
+  act(() => {
+    result.current.setGameId('uaR5XYbPZv5L54Zyibjq')
+    result.current.setUser('Anakin')
+  })
+  expect(result.current.gameId).toBe('uaR5XYbPZv5L54Zyibjq')
+  expect(result.current.user.name).toBe('Anakin')
+})

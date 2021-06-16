f38851ed306a4296a661c1b047be2adeb4381290
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-06-17 11:55:28 +1200

    create reducer for settings

---
+++ client/hooks/useSettingsReducer.tsx
@@ -0,0 +1,56 @@
+import { useReducer } from 'react'
+
+function reducer (state: State, action: Action) {
+  const result = { ...state }
+  // if settings change to tabletop, voteSystem must be spymaster-locksin
+  if (action.value === 'tabletop') {
+    result.voteSystem = 'spymaster-locksin'
+    result.gameplayMode = 'tabletop'
+    return result
+  } else if (action.setting === 'voteSystem' && result.gameplayMode === 'tabletop') {
+    result.error = 'Vote system must be controlled by spymaster in tabletop mode'
+    return result
+  } else {
+    result[action.setting as keyof Setting] = action.value
+    result.error = ''
+    return result
+  }
+}
+
+const initialState: State = {
+  voteSystem: 'vote',
+  gameplayMode: 'individual',
+  error: ''
+}
+
+export function useSettingsReducer (): useSettingsReducerReturn {
+  const [settings, dispatch] = useReducer(reducer, initialState)
+
+  const settingsDispatcher = (setting: Setting, value: Value) => {
+    dispatch({ setting, value })
+  }
+  return { settings, settingsDispatcher }
+}
+
+export type State = {
+  voteSystem: VoteSystem;
+  gameplayMode: GamePlayMode;
+  error?: string;
+}
+
+type VoteSystem = 'vote' | 'spymaster-locksin'
+type GamePlayMode = 'individual' | 'tabletop'
+
+type Action = {
+  setting: Setting
+  value: Value
+}
+
+type Setting = 'voteSystem' | 'gameplayMode'
+type Value = VoteSystem | GamePlayMode
+
+export type useSettingsReducerReturn = {
+  settings: State
+  // settingsDispatcher : React.Dispatch<Action>
+  settingsDispatcher: (setting: Setting, value: Value) => void
+}

f7eab58788d6a0308f16990ab4c1917d55c89684
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-07-19 19:49:20 +1200

    edit button to show when game not started

--- client/components/Game/StartGameButton.tsx
+++ client/components/Game/StartGameButton.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { useGameplayActions } from '../../contexts/FirebaseContext'
 import { useUserContext } from '../../contexts/UserContext'
 
-export const StartGameButton: React.FC = () => {
+export const StartGameButton: React.FC<Props> = ({ gameStart }) => {
   const { user, gameId } = useUserContext()
   const { startGame } = useGameplayActions()
 
   const handleStart = () => {
     startGame(gameId)
   }
-  if (user.host) {
+  if (user.host && !gameStart) {
     return (
       <button onClick={handleStart}>Start Game</button>
     )
   }
   return null
+}
+
+interface Props {
+  gameStart: boolean
 }

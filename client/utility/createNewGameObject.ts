0e455fcd2f88bbc540f062fefdc24f85ea39cbbc
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-07-19 17:25:28 +1200

    add restartGame function

--- client/utility/createNewGameObject.ts
+++ client/utility/createNewGameObject.ts
@@ -50,6 +50,42 @@ export const createGameObject = (user: User, teams: Teams, settings: Settings, l
   return newGameObject
 }
 
+export const restartGameState = (listWords: string[], teams = ['red', 'blue']): RestartGameStateReturn => {
+  const { boardKey, firstTurnTeam } = boardKeyCreator()
+  const teamPoints: TeamPoints = { red: 0, blue: 0 }
+  const scoresForWin: TeamPoints = { red: 0, blue: 0 }
+
+  teams.map((team) => {
+    if (team === firstTurnTeam) {
+      scoresForWin[team] = 9
+    } else {
+      scoresForWin[team] = 8
+    }
+    teamPoints[team] = 0
+  })
+
+  const boardObject = new Array(listWords.length)
+  for (const x in listWords) {
+    boardObject[x] = { index: parseInt(x), word: listWords[x], key: boardKey[x], revealed: false }
+  }
+
+  const gameState: GameState = {
+    gameStart: false,
+    guesses: 0,
+    teamPoints: teamPoints,
+    teamTurn: firstTurnTeam,
+    votes: [],
+    words: boardObject
+  }
+
+  return { gameState, scoresForWin }
+}
+
+type RestartGameStateReturn = {
+  gameState: GameState;
+  scoresForWin: TeamPoints;
+}
+
 type Teams = ['red', 'blue']
 
 export interface Settings {

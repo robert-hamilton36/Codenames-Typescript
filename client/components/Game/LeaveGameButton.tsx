2e78d5a624ea8f16ec0b1a0d53e40c8765597089
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-07-26 18:17:34 +1200

    add removeVotes when leaving

--- client/components/Game/LeaveGameButton.tsx
+++ client/components/Game/LeaveGameButton.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 
-import { useJoinGameActions } from '../../contexts/FirebaseContext'
+import { useJoinGameActions, useVoteActions } from '../../contexts/FirebaseContext'
 import { useUserContext } from '../../contexts/UserContext'
 
 export const LeaveGameButton: React.FC = () => {
   const { user, gameId } = useUserContext()
   const { leaveGame } = useJoinGameActions()
+  const { removePlayersVote } = useVoteActions()
   const history = useHistory()
 
   const handleClick = () => {
-    return leaveGame(user.uid, gameId)
+    return removePlayersVote(gameId, user.uid)
+      .then(() => leaveGame(user.uid, gameId))
       .then(() => history.push('/'))
   }
   return (

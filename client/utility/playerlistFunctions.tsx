6c3ca533dddbf64e183535c8ca8ed91068f128df
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-08-18 15:30:58 +1200

    rework functions so they work the same with no players

--- client/utility/playerlistFunctions.tsx
+++ client/utility/playerlistFunctions.tsx
@@ -8,23 +8,26 @@ export const makePlayerTableRows: MakePlayerTables = (playerlist: User[], setSel
   const blueOperatives = playerlist.filter(player => !player.spymaster && player.team === 'blue')
   const redOperatives = playerlist.filter(player => !player.spymaster && player.team === 'red')
 
-  // find which team has the most players, and so the number of rows to make
-  const operativeRows = blueOperatives.length > redOperatives.length ? blueOperatives.length : redOperatives.length
+  // find which team has the most players, which is the number of rows to make
+  const noOfOperativeRows = blueOperatives.length > redOperatives.length ? blueOperatives.length : redOperatives.length
   const operativeTable: JSX.Element[] = []
-  for (let x = 0; x < operativeRows; x++) {
+  for (let x = 0; x < noOfOperativeRows; x++) {
     operativeTable.push(
-      <tr key={'operativeRow' + x}>
-        {redOperatives[x] ? <td key={'red' + x} onClick={() => setSelectedPlayer(redOperatives[x])} >{redOperatives[x].name}</td> : <td className = 'noOperative'></td>}
-        {blueOperatives[x] ? <td key={'blue' + x} onClick={() => setSelectedPlayer(blueOperatives[x])} >{blueOperatives[x].name}</td> : <td className = 'noOperative'></td>}
+      <tr key={'operativeRow' + x} data-testid = {'operativeRow' + x}>
+        {redOperatives[x] ? <td key={'red' + x} onClick={() => setSelectedPlayer(redOperatives[x])} >{redOperatives[x].name}</td> : <td className = 'noOperative' data-testid = {'noRedOperative' + x}></td>}
+        {blueOperatives[x] ? <td key={'blue' + x} onClick={() => setSelectedPlayer(blueOperatives[x])} >{blueOperatives[x].name}</td> : <td className = 'noOperative' data-testid = {'noBlueOperative' + x}></td>}
       </tr>)
   }
 
-  const spymasterTable = (
-    <tr>
-      {redSpymaster ? <td onClick={() => setSelectedPlayer(redSpymaster)}>{redSpymaster.name}</td> : <td></td>}
-      {blueSpymaster ? <td onClick={() => setSelectedPlayer(blueSpymaster)}>{blueSpymaster.name}</td> : <td></td>}
-    </tr>
-  )
+  let spymasterTable: JSX.Element
+  if (blueSpymaster || redSpymaster) {
+    spymasterTable = (
+      <tr>
+        {redSpymaster ? <td onClick={() => setSelectedPlayer(redSpymaster)}>{redSpymaster.name}</td> : <td className = 'noSpymaster' data-testid = 'noRedSpymaster'></td>}
+        {blueSpymaster ? <td onClick={() => setSelectedPlayer(blueSpymaster)}>{blueSpymaster.name}</td> : <td className = 'noSpymaster' data-testid = 'noBlueSpymaster'></td>}
+      </tr>
+    )
+  }
 
   return [spymasterTable, operativeTable]
 }

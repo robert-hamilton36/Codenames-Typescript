ca1113206438f3bb486d6975b3aef4ebf7bf7e26
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2021-09-12 12:39:44 +1200

    add new function test

--- client/components/Hello.tsx
+++ client/components/Hello.tsx
@@ -1,41 +1,57 @@
 import React from 'react'
-import { useFirebase, useFirestoreSubscriber } from '../contexts/FirebaseContext'
 
-import { useUserContext } from '../contexts/UserContext'
+import { useGameplayActions, useGuessActions, useFirestoreSubscriber, getWords } from '../contexts/FirebaseContext'
+import { useUserActions, useUserContext } from '../contexts/UserContext'
+import { restartGameState } from '../utility/createNewGameObject'
+import { shuffleArray } from '../utility/shuffleArray'
 
 export const Hello: React.FC = () => {
-  const { user, setUser, gameId } = useUserContext()
-  const { actions } = useFirebase()
-
+  const { user } = useUserContext()
+  const { setUser } = useUserActions()
+  const gameId = 'Test'
   const data = useFirestoreSubscriber(gameId)
+  const { startGame, restartNewGame, setHint, changeTeamsTurn, teamWinGame } = useGameplayActions()
+  const { endTurn, makeGuess } = useGuessActions()
+  // const { } = useVoteActions()
+  const words = getWords()
 
-  const handleClick = () => {
-    actions.createGame({ players: [user], settings: { gameplayMode: 'individual' } })
-  }
-
-  const handleJoin = () => {
-    actions.joinGame({ name: 'Obi Wan', uid: '34342-fad-231' }, gameId)
-  }
-
-  const handleKick = () => {
-    actions.kickPlayer('34342-fad-231', gameId)
-  }
-
-  const handleLeave = () => {
-    actions.leaveGame(user.uid, gameId)
-  }
-
-  const handleDelete = () => {
-    actions.deleteGame(gameId)
+  const handleStart = () => {
+    startGame(gameId)
   }
 
   const handleLogin = () => {
-    setUser('Anakin')
+    setUser('Katie')
   }
 
-  const handleLogOut = () => {
-    setUser('')
+  const handleHint = () => {
+    setHint(gameId, { hint: 'fire', numberOfWords: 2 })
   }
+
+  const handleChangeTeamTurn = (team) => {
+    changeTeamsTurn(gameId, team)
+  }
+
+  const handleRestart = () => {
+    const wordGrid = shuffleArray(words)
+    console.log(wordGrid.slice(24))
+    const { gameState, scoresForWin } = restartGameState(wordGrid.slice(0, 25))
+    console.log(gameState)
+    console.log(scoresForWin)
+    restartNewGame(gameId, gameState, scoresForWin)
+  }
+
+  const handleTeamWin = (team) => {
+    teamWinGame(gameId, team)
+  }
+
+  const handleWrongGuess = () => {
+    endTurn(gameId)
+  }
+
+  const handleReveal = () => {
+    makeGuess(gameId, 0)
+  }
+
   return (
     <>
       <h1>App has arrived</h1>
@@ -44,13 +60,16 @@ export const Hello: React.FC = () => {
       {user && <h1>{user.uid}</h1>}
       {gameId && <h1>{gameId}</h1>}
       {data && data.players.map((player) => <h1 key={player.uid}>{player.name}</h1>)}
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogOut}>LogOut</button>
-      <button onClick={handleClick}>Create game</button>
-      <button onClick={handleJoin}>Join</button>
-      <button onClick={handleKick}>Kick</button>
-      <button onClick={handleLeave}>Leave</button>
-      <button onClick={handleDelete}>Delete</button>
+      {!user.name && <button onClick={handleLogin}>Login</button>}
+      {data && !data.gameState.gameStart && <button onClick={handleStart}>Start Game</button>}
+      {data && !data.gameState.hint && <button onClick={handleHint}>Add hint</button>}
+      {data && data.gameState.teamTurn === 'red' && <button onClick={() => handleChangeTeamTurn('blue')}>Change Team Blue</button>}
+      {data && data.gameState.teamTurn === 'blue' && <button onClick={() => handleChangeTeamTurn('red')}>Change Team Red</button>}
+      <button onClick={() => handleTeamWin('blue')}>Blue Win</button>
+      <button onClick={() => handleTeamWin('red')}>Red Win</button>
+      <button onClick={handleWrongGuess}>Guess Wrong</button>
+      <button onClick={handleReveal}>Guess 0</button>
+      <button onClick={handleRestart}>Restart</button>
     </>
   )
 }

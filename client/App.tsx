c855e390e495320bec0f39f14a2700e03e97dc4e
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2022-08-05 12:11:21 +1200

    remove troubleshooting

--- client/App.tsx
+++ client/App.tsx
@@ -4,12 +4,6 @@ import { Providers } from './Providers'
 import { Routes } from './Routes'
 
 const App: React.FC = () => {
-  console.log(process.env.apiKey)
-  console.log(process.env.authDomain)
-  console.log(process.env.projectId)
-  console.log(process.env.storageBucket)
-  console.log(process.env.messagingSenderId)
-  console.log(process.env.appId)
   return (
     <Providers>
       <Routes/>
